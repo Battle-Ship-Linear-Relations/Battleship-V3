dd3f2a2b977fc8afe857d2a3eedd4586dda32003
Manandua17 <120732926+Manandua17@users.noreply.github.com>
2023-01-26 22:35:58 -0500

    Update battleship.java

--- battleship.java
+++ battleship.java
@@ -1,3 +1,7 @@
+/**
+ * @author duamanan
+ * @author GeldelaMohit
+ */
 class battleship {
     public static void main(String[] args){
         new gameFrame(); // create a JFrame through class gameFrame
